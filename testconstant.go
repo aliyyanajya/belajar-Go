b7349e57649cec11fdebce739003163c0dfdeecb
aliya ruby <aliya.aurelia@gmail>
2019-03-01 13:33:30 +0700

    menambah file constant menghapus file testvariable dan ubah variables2

---
+++ testconstant.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+//const alamat string = "Bandung"
+
+func main() {
+	const alamat string = "Bandung"
+	/* Pembubuhan value 1 baris */
+	var nama string = "David Maulana"
+	fmt.Println(nama)
+
+	/* Pembubuhan value baris berikutnya */
+	var umur int
+	umur = 20
+	fmt.Println(umur)
+
+	//alamat := "Jakarta"
+	fmt.Println(alamat)
+
+	//alamat = "Surabaya"
+	fmt.Println(alamat)
+
+	//alamat = 12
+	fmt.Println(alamat)
+
+}
--- tesvariables2.go
+++ tesvariables2.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
+/*
 var (
 	nama   = "David Maulana"
 	umur   = 17
 	alamat = "Jakarta"
 )
+*/
 
 func main() {
+
+	nama, umur, alamat := "David Maulana", 17, "Jakarta"
 
 	fmt.Printf("%s berumur %d tahun\n", nama, umur)
 	fmt.Println("Beralamat di ", alamat)

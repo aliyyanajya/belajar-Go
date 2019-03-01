3c8c3f27e88bd7d19d6408af54a9f9957ebb3734
aliya ruby <aliya.aurelia@gmail>
2019-03-01 14:25:26 +0700

    menambah file declarasi variable dan print return error

---
+++ declarevariable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 8
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 99 + 88
+	fmt.Println(y)
+}
---
+++ printreterr.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//n, err := fmt.Println("hallo teman-teman", 42, true)
+	n, _ := fmt.Println("hallo teman-teman", 42, true)
+	fmt.Println(n)
+	//fmt.Println(err)
+}
